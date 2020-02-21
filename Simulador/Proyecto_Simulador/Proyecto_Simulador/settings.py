3a4c76556102b8731df65d8801ea9fa76be0b6ab
gonzalo194753 <satrusteguigonzalo@gmail.com>
2020-02-21 20:06:54 +0100

    Subir django app

--- Simulador/Proyecto_Simulador/Proyecto_Simulador/settings.py
+++ Simulador/Proyecto_Simulador/Proyecto_Simulador/settings.py
@@ -73,12 +73,7 @@ WSGI_APPLICATION = 'Proyecto_Simulador.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
---
+++ Simulador/Proyecto_Simulador/SimuladorApp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SimuladorappConfig(AppConfig):
+    name = 'SimuladorApp'
