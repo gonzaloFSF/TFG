f6ef5ffa5f7fc7c0363825115fda57519a4aa95d
gonzalo194753 <satrusteguigonzalo@gmail.com>
2020-02-21 16:25:45 +0100

    Subir cambios a github

---
+++ jump_line_detect.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+
+int main(){
+
+int i;
+int j;
+int a;
+
+a = 0;
+
+if(a == 20){
+
+a++;
+}
+
+for(i = 0; i < 1000 ; i++){
+for(j = 0; j < 10; j++){
+a += 1;
+
+}
+
+}
+
+
+
+
+}
+
