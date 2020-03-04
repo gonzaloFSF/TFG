5ef40d69f722e1e512e849e019a95e1fe94c0374
gonzalo <satrusteguigonzalo@gmailcom>
2020-03-04 16:26:54 +0100

    Arreglar fallo en url.py

--- Simulador/Proyecto_Simulador/Proyecto_Simulador/urls.py
+++ Simulador/Proyecto_Simulador/Proyecto_Simulador/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, logout_then_login
-from gest_jmb_app.views import index
+from SimuladorApp.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
