b68e3cb06fbf1fdce25d10b5d18af47598995736
gonzalo <satrusteguigonzalo@gmailcom>
2020-03-09 12:19:42 +0100

    Prueba variable sesion

--- Simulador/Proyecto_Simulador/SimuladorApp/views.py
+++ Simulador/Proyecto_Simulador/SimuladorApp/views.py
@@ -4,4 +4,16 @@ from django.shortcuts import render
 
 
 def index(request):
+    request.session['test'] = 'prueba'
     return render(request, 'generic/index.html')
+
+
+def display_simulador(request):
+    pass
+
+def display_traza(request):
+    pass
+
+
+
+# git add . && git commit -m "Prueba variable sesion" && git push
