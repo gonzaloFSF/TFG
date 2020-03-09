aa68bc6e74fcdf903b385b67d50a375f81ece9bf
gonzalo <satrusteguigonzalo@gmailcom>
2020-03-09 12:23:43 +0100

    Añadir sqlite3

--- Simulador/Proyecto_Simulador/Proyecto_Simulador/settings.py
+++ Simulador/Proyecto_Simulador/Proyecto_Simulador/settings.py
@@ -99,6 +99,18 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
