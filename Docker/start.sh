0ea016eede971f1c565e903360ffcd083006e09e
gonzalo194753 <satrusteguigonzalo@gmail.com>
2020-04-07 14:46:31 +0200

    Subir archivos docker

---
+++ Docker/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+service ssh start
+
+git clone git@github.com:gonzaloFSF/TFG.git /var/www/TFG
+
+cd /var/www/TFG/Simulador/
+pip3 install -r requirements.txt
+cd ./Proyecto_Simulador
+python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py runserver localhost:9999 &
+
+cd
+apachectl -DFOREGROUND
+
+
+
